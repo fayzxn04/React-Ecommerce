fe08f2fb32d04df19d063bd74017cbcfa60d311e
Fayzan Shaikh <iamfaizan98@gmail.com>
2025-04-20 17:36:28 +0530

    Implement Grid and List view Layout

---
+++ src/context/filter_context.js
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useProductContext } from "./productcontext";
+import reducer from "../reducer/filterReducer";
+
+const FilterContext = createContext();
+
+const initialState = {
+  filter_products: [],
+  all_products: [],
+  grid_view: false,
+};
+
+export const FilterContextProvider = ({ children }) => {
+  const { products } = useProductContext();
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //   setting grid view
+
+  const setGridView = () => {
+    return dispatch({ type: "SET_GRID_VIEW" });
+  };
+
+  //   setting List view
+
+  const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
+  }, [products]);
+
+  return (
+    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilterContext = () => {
+  return useContext(FilterContext);
+};
