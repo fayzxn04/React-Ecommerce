08544d41243cb57552e094beb1a965208c74e9cd
Fayzan Shaikh <iamfaizan98@gmail.com>
2025-04-21 23:47:50 +0530

    Implement the Product Page

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,7 +7,17 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
+  sorting_value: "lowest",
+  filters: {
+    text: "",
+    category: "all",
+    company: "all",
+    color: "all",
+    maxPrice: 0,
+    price: 0,
+    minPrice: 0,
+  },
 };
 
 export const FilterContextProvider = ({ children }) => {
@@ -15,24 +25,48 @@ export const FilterContextProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  //   setting grid view
+  //   Setting Grid view
 
   const setGridView = () => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
 
-  //   setting List view
+  //   Setting List view
 
   const setListView = () => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
+
+  // Sorting Function
+
+  const sorting = (event) => {
+    let userValue = event.target.value;
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
+  };
+
+  // Update the Filter Values
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    return dispatch({ type: "UPDATE_FILTER_VALUE", payload: { name, value } });
+  };
+
+  // To sort the Products !
+
+  useEffect(() => {
+    dispatch({ type: "FILTER_PRODUCTS" });
+    dispatch({ type: "SORTING_PRODUCTS" });
+  }, [products, state.sorting_value, state.filters]);
 
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, sorting, updateFilterValue }}
+    >
       {children}
     </FilterContext.Provider>
   );
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,10 +1,15 @@
 const filterReducer = (state, action) => {
   switch (action.type) {
     case "LOAD_FILTER_PRODUCTS":
+      let priceArr = action.payload.map((curElem) => curElem.price);
+
+      let maxPrice = Math.max(...priceArr);
+
       return {
         ...state,
         filter_products: [...action.payload],
         all_products: [...action.payload],
+        filters: { ...state.filters, maxPrice, price: maxPrice },
       };
 
     case "SET_GRID_VIEW":
@@ -17,6 +22,96 @@ const filterReducer = (state, action) => {
       return {
         ...state,
         grid_view: false,
+      };
+
+    case "GET_SORT_VALUE":
+      return {
+        ...state,
+        sorting_value: action.payload,
+      };
+
+    case "SORTING_PRODUCTS":
+      let newSortData;
+
+      const { filter_products, sorting_value } = state;
+      let tempSortProduct = [...filter_products];
+
+      const sortingProducts = (a, b) => {
+        if (sorting_value === "lowest") {
+          return a.price - b.price;
+        }
+
+        if (sorting_value === "highest") {
+          return b.price - a.price;
+        }
+
+        if (sorting_value === "a-z") {
+          return a.name.localeCompare(b.name);
+        }
+
+        if (sorting_value === "z-a") {
+          return b.name.localeCompare(a.name);
+        }
+      };
+
+      newSortData = tempSortProduct.sort(sortingProducts);
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
+
+    case "UPDATE_FILTER_VALUE":
+      const { name, value } = action.payload;
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+      };
+
+    case "FILTER_PRODUCTS":
+      let { all_products } = state;
+      let tempFilterProduct = [...all_products];
+
+      const { text, category, company, color, price } = state.filters;
+      if (text) {
+        tempFilterProduct = tempFilterProduct.filter((curElm) => {
+          return curElm.name.toLowerCase().includes(text);
+        });
+      }
+
+      if (category !== "all") {
+        tempFilterProduct = tempFilterProduct.filter((curElm) => {
+          return curElm.category === category;
+        });
+      }
+
+      if (company !== "all") {
+        tempFilterProduct = tempFilterProduct.filter((curElm) => {
+          return curElm.company.toLowerCase() === company.toLowerCase();
+        });
+      }
+
+      if (color !== "all") {
+        tempFilterProduct = tempFilterProduct.filter((curColor) => {
+          return curColor.colors.includes(color);
+        });
+      }
+
+      if (price === 0) {
+        tempFilterProduct = tempFilterProduct.filter(
+          (curElem) => curElem.price == price
+        );
+      } else {
+        tempFilterProduct = tempFilterProduct.filter(
+          (curElem) => curElem.price <= price
+        );
+      }
+
+      return {
+        ...state,
+        filter_products: tempFilterProduct,
       };
 
     default:
